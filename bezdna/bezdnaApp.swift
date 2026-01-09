72fc8e30fa3093b1c68570a15b7f03225df7bc12
qwe <sdfdsf@sdd>
2026-01-09 19:38:04 +0300

    Initial Commit

---
+++ bezdna/bezdnaApp.swift
@@ -0,0 +1,17 @@
+//
+//  bezdnaApp.swift
+//  bezdna
+//
+//  Created by Andrei Brizhinev on 09.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct bezdnaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bezdnaTests/bezdnaTests.swift
@@ -0,0 +1,17 @@
+//
+//  bezdnaTests.swift
+//  bezdnaTests
+//
+//  Created by Andrei Brizhinev on 09.01.2026.
+//
+
+import Testing
+@testable import bezdna
+
+struct bezdnaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
